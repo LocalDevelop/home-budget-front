b513541af83d70890974a6d147cd42813bc6b6e2
liya19 <liya1999.19@yandex.ru>
2022-07-30 22:11:12 +0300

    created login page on /login

--- frontend/home_budget_front/src/App.js
+++ frontend/home_budget_front/src/App.js
@@ -5,12 +5,10 @@ import {
   Routes,
 } from 'react-router-dom';
 
-
-
 export const App = () => (
   <Router>
       <Routes>
-        {/* <Route exact path="/" element={<Home/>} /> */}
+        {/* <Route exact path="/" element={<Home />} /> */}
         <Route exact path="/login" element={<SignIn />} />
       </Routes>
   </Router>
--- frontend/home_budget_front/src/Components/SignIn/SignIn.js
+++ frontend/home_budget_front/src/Components/SignIn/SignIn.js
@@ -1,18 +1,52 @@
+import TextField from '@mui/joy/TextField';
+import Card from '@mui/joy/Card';
+import Button from '@mui/joy/Button';
+import Typography from '@mui/joy/Typography';
+import TelegramIcon from '@mui/icons-material/Telegram';
+
+
+
+
 export const SignIn = () => {
+
     return (
-        <div>
-            <form>
-                <label>
-                    Логин:
-                    <input type="text" name="name" />
-                </label>
-                <label>
-                    Пароль:
-                    <input type="password" name="password" />
-                </label>
-                <input type="submit" value="Отправить" />
-            </form>
-        </div>
+        <Card variant="outlined" sx={{
+            width: '300px',
+            borderColor: 'gray',
+            display: 'grid',
+            gap: 1,
+            alignItems: 'center',
+            justifyItems: 'center',
+            left: '35%',
+            top: '200px',
+            flex: 'wrap'
+        }}>
+
+            <TextField
+                placeholder="Login"
+                variant="outlined"
+                color="primary"
+                fullWidth
+            />
+            <TextField
+                placeholder="Password"
+                type="password"
+                variant="outlined"
+                color="primary"
+                fullWidth
+            />
+            <Typography variant="caption" display="block" gutterBottom textColor="gray" textAlign="center">
+                или
+            </Typography>
+
+            <TelegramIcon onclick color="primary"></TelegramIcon>
+
+            <Button
+                color="primary"
+                fullWidth
+                variant="outlined">Войти
+            </Button>
+        </Card>
     )
 }
 export default SignIn;
